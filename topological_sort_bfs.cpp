555bb6f971a3f39df4fbe2072bb9abde28a817ec
samarth1345 <76591895+samarth1345@users.noreply.github.com>
2022-05-03 23:23:25 +0530

    Update topological_sort_bfs.cpp

--- topological_sort_bfs.cpp
+++ topological_sort_bfs.cpp
@@ -1,8 +1,40 @@
 //kahn's algorithm
 //only for DAG
 #include <bits/stdc++.h>
-#include "graph_list.h"
 using namespace std;
+class graph
+{
+    int no_of_vertices;
+    vector<int> List[MAX];
+public:
+    graph(int val)
+    {
+        no_of_vertices = val;
+    }
+    void input(int vertice1, int vertice2)
+    {
+        List[vertice1].push_back(vertice2);
+        List[vertice2].push_back(vertice1);
+    }
+    void input_directed(int vertice1, int vertice2)
+    {
+        List[vertice1].push_back(vertice2);
+    }
+    void print()
+    {
+        for(int i=1;i<=no_of_vertices;i++)
+        {
+            cout<<i<<": ";
+            for(int j=0;j<List[i].size();j++)
+                cout<<List[i][j]<<" ";
+            cout<<endl;
+        }
+    }
+    vector<int> *getList()
+    {
+        return List;
+    }
+};
 void kahns(vector<int> *List,int vertices)
 {
     vector<int> indegree(vertices);
